3af730ba4dc20afe1011b89ad67a1c546cd012e1
Proft <b.proft@eathappygroup.com>
2024-07-03 13:40:17 +0200

    first commit

---
+++ functions.py
@@ -0,0 +1,14 @@
+import random
+
+def functionName():
+    print("test")
+
+functionName()
+
+def celsiusToFahrenheit(c):
+    return round(1.8*c+32)
+
+# celsius = float(input("Celsius\n"))
+# print(str(celsiusToFahrenheit(celsius)))
+
+print(random.randint(1,2))
---
+++ p1.py
@@ -0,0 +1,22 @@
+print("Test")
+
+test = 5;
+test2 = test-4
+
+test = "X"
+print(test)
+
+
+sliced = "String" + "XXX"
+print(sliced[2])
+print(sliced[2:5])
+
+
+print(type(sliced))
+print(type(str(sliced)))
+print(type(5))
+
+inp = input("Something\n")
+print(int("44"))
+
+
